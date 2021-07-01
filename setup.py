7c5699c41c92bd09f7a7883dd54440272c4d7cd0
kxu112 <84808479+kxu112@users.noreply.github.com>
2021-07-01 16:53:22 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ceaos_temphumidity',
+    version='0.0.1',
+    packages=['ceaos_temphumidity'],
+    install_requires=[
+        'pigpio',
+    ],
+)
